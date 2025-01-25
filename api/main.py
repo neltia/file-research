ae70b2eca08d70c60654a34040299e1d6ec8343c
Hyunseo <dsz08082@naver.com>
2025-01-25 21:25:05 +0900

    test: 배포 설정 변경 테스트

--- api/main.py
+++ api/main.py
@@ -36,4 +36,4 @@ app.include_router(file_info.router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=8000)
